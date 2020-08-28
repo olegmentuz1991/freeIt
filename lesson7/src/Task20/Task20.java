2a06a4fb377e967ed26a5218d6125b83dd9adbb4
Oleg Mentuz <olegmentuz@gmail.com>
2020-08-28 12:26:34 +0300

    Task20

--- lesson7/src/Task20/Task20.java
+++ lesson7/src/Task20/Task20.java
@@ -1,4 +1,16 @@
 package Task20;
-
+//Имеется строка с текстом. Вывести текст, составленный из последних букв
+//всех слов.
 public class Task20 {
-}
+    public static void main(String[] args) {
+            String str = "Имеется строка с текстом";
+            StringBuilder res = new StringBuilder();
+
+            String [] words = str.split("\\s+");
+            for ( String word : words )
+                res.append(word.toCharArray()[word.length() - 1]);
+            System.out.println("Result: " + res.toString());
+        }
+    }
+
+
